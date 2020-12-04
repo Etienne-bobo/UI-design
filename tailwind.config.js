0cc3a1d5e0a904dde4925abd5f30e7297690fdd0
Etienne-bobo <etiennebobo17@gmail.com>
2020-12-04 13:21:16 +0530

    first_design

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './src/App.vue'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
